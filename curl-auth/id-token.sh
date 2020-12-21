3cbf3258a4ae6da6bb4c7f0719ba6bd08e5bd101
Isuru Gunawardana <igunawardana87@gmail.com>
2020-12-21 07:14:28 +0530

    adding url

--- curl-auth/id-token.sh
+++ curl-auth/id-token.sh
@@ -1,6 +1,6 @@
-command="curl -k --user $CLIENT_ID:$CLIENT_SECRET  $TOKEN_EP -d  code=$CODE&grant_type=authorization_code&client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI"
+command="curl https://localhost:9443/oauth2/token -k --user $CLIENT_ID:$CLIENT_SECRET  $TOKEN_EP -d  code=$CODE&grant_type=authorization_code&client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI"
 echo "Executing the command..."
 echo $command
 echo
 $command
-echo 
+echo
